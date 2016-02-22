fd5b1096452e3c5431ea96fdc226169f277b0ae2
ChengeLi <cl2840@nyu.edu>
2016-02-22 11:35:44 -0500

    fitting and smoothing added

--- fit_extrapolate.py
+++ fit_extrapolate.py
@@ -6,103 +6,20 @@ import numpy as np
 from scipy.io import loadmat,savemat
 from scipy.sparse import csr_matrix
 import matplotlib.pyplot as plt
+from scipy.interpolate import spline
+from scipy.interpolate import interp1d
+from scipy.stats import norm
+from scipy.interpolate import InterpolatedUnivariateSpline
+from sklearn.cluster import KMeans
+from mpl_toolkits.mplot3d import Axes3D
+from sets import Set
+
+
+
 # from DataPathclass import *
 # DataPathobj = DataPath(VideoIndex)
 
-# define start and end regions
-
-#Canal video's dimensions:
-"""(528, 704, 3)
-start :<=110,
-end: >=500,"""
-
-start_Y = 110;
-end_Y   = 500;
-
-matfilepath    = '/Users/Chenge/Desktop/testklt/'
-matfiles       = sorted(glob.glob(matfilepath + 'klt_*.mat'))
-start_position = 0 
-matfiles       = matfiles[start_position:]
-
-
-# for matidx,matfile in enumerate(matfiles):
-matidx = 0
-matfile = matfiles[matidx]
-# print "Processing truncation...", str(matidx+1)
-ptstrj = loadmat(matfile)
-mask = ptstrj['trjID'][0]
-x    = csr_matrix(ptstrj['xtracks'], shape=ptstrj['xtracks'].shape).toarray()
-y    = csr_matrix(ptstrj['ytracks'], shape=ptstrj['ytracks'].shape).toarray()
-t    = csr_matrix(ptstrj['Ttracks'], shape=ptstrj['Ttracks'].shape).toarray()
-
-# if consecutive points are similar to each other, merge them, using one to represent
-Ntrj = len(x)
-plt.figure()
-# for ii in range(0,x.shape[0],100):
-# 	# plt.plot(x[ii,:][x[ii,:]!=0])
-# 	# plt.plot(y[ii,:][y[ii,:]!=0])
-# 	plt.plot(x[ii,:][x[ii,:]!=0],y[ii,:][y[ii,:]!=0])
-
-# xspd = np.zeros((x.shape[0],x.shape[1]-1))
-badTrj = []
-goodTrj = []
-p3 = []
-for kk in range(Ntrj):
-	xk = x[kk,:][x[kk,:]!=0]
-	yk = y[kk,:][y[kk,:]!=0]
-	xspd = np.diff(xk[xk!=0])
-	xaccelerate = np.diff(xspd)
-	yspd = np.diff(yk[yk!=0])
-	yaccelerate = np.diff(yspd)
-	# print sum(xspd)
-	# print sum(accelerate)
-	# if (np.sum(xspd)<=1e-2 and np.sum(xaccelerate)<=1e-1) or (np.sum(yspd)<=1e-2 and np.sum(yaccelerate)<=1e-1):
-	# if len(xspd)<=3 and (np.sum(xspd)<=1e-2) and (np.sum(xaccelerate)<=1e-1):
-	if ((np.abs(np.sum(xspd))<=1e-2) and (np.abs(np.sum(yspd))<=1e-2)) or ((np.max(xk)-np.min(xk)<=5) and (np.max(yk)-np.min(yk)<=5)):
-	# if len(xspd)<=5:	
-		# print "xspd",xspd
-		# print "yspd",yspd
-		badTrj.append(kk)
-		# plt.plot(x[kk,:][x[kk,:]!=0],y[kk,:][y[kk,:]!=0])
-		# pdb.set_trace()
-	else:
-		goodTrj.append(kk)
-		# pdb.set_trace()
-		p3.append(np.polyfit(x[kk,:][x[kk,:]!=0], y[kk,:][y[kk,:]!=0], 3))  # fit a poly line to the last K points
-
-p3 = np.array(p3)
-
-plt.ion()
-plt.figure()
-for ii in range(0,len(goodTrj),1):
-	kk = goodTrj[ii]
-	xk = x[kk,:][x[kk,:]!=0]
-	yk = y[kk,:][y[kk,:]!=0]
-	if len(xk)>10:
-		# plt.plot(xk)
-		# plt.plot(yk)
-		# plt.plot(xk, yk)
-		# smooth(xk, yk)
-		# extraPolate(xk, yk)
-		x_fit = np.linspace(xk.min(), xk.max(), 200)
-		y_fit = pow(x_fit,3)*p3[ii,0] + pow(x_fit,2)*p3[ii,1] + pow(x_fit,1)*p3[ii,2]+ p3[ii,3]
-		plt.plot(x_fit, y_fit)
-		plt.draw()
-plt.show()
-
-outlierID =[]
-for ii in range(p3.shape[1]):
-	data = p3[:,ii]
-	mu,std = fitGaussian(data)
-	outlierID = outlierID + list(np.where(data>=mu+2*std)[0])+list(np.where(data<=mu-2*std)[0])
-	print p3[outlierID,:]
-outlierID = np.unique(outlierID)
-
-
-
-
 # fitting Gaussian and get rid of the outlier(too large p3)
-from scipy.stats import norm
 def fitGaussian(data):
 	# Fit a normal distribution to the data:
 	mu, std = norm.fit(data)
@@ -119,19 +36,52 @@ def fitGaussian(data):
 	return mu, std
 
 
+def polyFitTrj(x,y):
+	badTrj  = []
+	goodTrj = []
+	p3      = [] #polynomial coefficients, order 3
+	for kk in range(x.shape[0]):
+		xk = x[kk,:][x[kk,:]!=0]
+		yk = y[kk,:][y[kk,:]!=0]
+		xspd = np.diff(xk[xk!=0])
+		xaccelerate = np.diff(xspd)
+		yspd = np.diff(yk[yk!=0])
+		yaccelerate = np.diff(yspd)
+		# print sum(xspd)
+		# print sum(accelerate)
+		# if (np.sum(xspd)<=1e-2 and np.sum(xaccelerate)<=1e-1) or (np.sum(yspd)<=1e-2 and np.sum(yaccelerate)<=1e-1):
+		# if len(xspd)<=3 and (np.sum(xspd)<=1e-2) and (np.sum(xaccelerate)<=1e-1):
+		if ((np.abs(np.sum(xspd))<=1e-2) and (np.abs(np.sum(yspd))<=1e-2)) or ((np.max(xk)-np.min(xk)<=5) and (np.max(yk)-np.min(yk)<=5)):
+		# if len(xspd)<=5:	
+			# print "xspd",xspd
+			# print "yspd",yspd
+			badTrj.append(kk)
+			# plt.plot(x[kk,:][x[kk,:]!=0],y[kk,:][y[kk,:]!=0])
+			# pdb.set_trace()
+		else:
+			goodTrj.append(kk)
+			# pdb.set_trace()
+			p3.append(np.polyfit(x[kk,:][x[kk,:]!=0], y[kk,:][y[kk,:]!=0], 3))  # fit a poly line to the last K points
 
+	outlierID =[]
+	p3 = np.array(p3)
+	goodTrj = np.array(goodTrj)
+	for ii in range(p3.shape[1]):
+		data = p3[:,ii]
+		outlierID = outlierID+ list(np.where(np.isnan(data)==True)[0])
 
-
-
-
-
-
-
-
+		mu,std = fitGaussian(data[np.ones(len(data), dtype=bool)-np.isnan(data)])
+		outlierID = outlierID + list(np.where(data>=mu+std)[0])+list(np.where(data<=mu-std)[0])
+		# print p3[outlierID,:]
+	outlierID = np.unique(outlierID)
+	TFid = np.ones(len(goodTrj),'bool')
+	TFid[outlierID] = False
+	goodTrj = goodTrj[TFid]
+	p3      = p3[TFid]
+	return np.array(p3),np.array(goodTrj)
 
 
 # extrapolate original trj
-from scipy.interpolate import InterpolatedUnivariateSpline
 def extraPolate(xk, yk):
 	# positions to inter/extrapolate
 	# y_extraPosistion = np.linspace(start_Y, end_Y, 2)
@@ -153,11 +103,6 @@ def extraPolate(xk, yk):
 	#     plt.plot(x, y)
 	plt.show()
 
-
-
-from scipy.interpolate import spline
-from scipy.interpolate import interp1d
-
 def smooth(xk, yk):
 	# # x_smooth = np.linspace(xk.min(), xk.max(), 200)
 	# # y_smooth = spline(xk, yk, x_smooth,order = 2)
@@ -167,40 +112,134 @@ def smooth(xk, yk):
 	# x_smooth = s(y_smooth)
 	# plt.plot(x_smooth, y_smooth, linewidth=1)
 
-	f = interp1d(xk, yk, kind='linear', axis=-1, copy=True, bounds_error=True, fill_value=np.nan, assume_sorted=False)
-	xnew = np.arange(xk.min(), xk.max(),1)
-	ynew = f(xnew)
-	plt.plot(xnew, ynew, linewidth=1)
-	plt.draw()
+	f1 = interp1d(xk, yk, kind='linear', axis=-1, copy=True, bounds_error=True, fill_value=np.nan, assume_sorted=False)
+	x_smooth_per_pixel = np.arange(xk.min(), xk.max(),0.5)
+	y_smooth_per_pixel = f1(x_smooth_per_pixel)
+	x_smooth_same_len = np.linspace(x_smooth_per_pixel.min(), x_smooth_per_pixel.max(),len(xk))
+	f2 = interp1d(x_smooth_per_pixel, y_smooth_per_pixel, kind='slinear', axis=-1, copy=True, bounds_error=True, fill_value=np.nan, assume_sorted=False)
+	y_smooth_same_len = f2(x_smooth_same_len)
+	# plt.plot(x_smooth, y_smooth, linewidth=1)
+	# plt.draw()
 	# pdb.set_trace()
-	# return x_smooth, y_smooth
-
+	return x_smooth_same_len, y_smooth_same_len
 
 
 # k-means on polynomial coefs
-from sklearn.cluster import KMeans
-from mpl_toolkits.mplot3d import Axes3D
-np.random.seed(5)
-estimators = {'k_means_20': KMeans(n_clusters=20),
-              'k_means_8': KMeans(n_clusters=8),
-              'k_means_iris_bad_init': KMeans(n_clusters=30, n_init=1,
-                                              init='random')}
+def kmeansPolyCoeff(p3):
+	np.random.seed(5)
+	estimators = {'k_means_20': KMeans(n_clusters=20),
+	              'k_means_8': KMeans(n_clusters=8),
+	              'k_means_bad_init': KMeans(n_clusters=30, n_init=1,
+	                                              init='random')}
 
-fignum = 1
-for name, est in estimators.items():
-    est.fit(p3)
-    labels = est.labels_
-    fig = plt.figure(fignum, figsize=(4, 3))
-    plt.clf()
-    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+	fignum = 1
+	for name, est in estimators.items():
+	    est.fit(p3)
+	    labels = est.labels_
+	    fig = plt.figure(fignum, figsize=(4, 3))
+	    plt.clf()
+	    plt.title(str(name))
+	    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
 
-    plt.cla()
-    ax.scatter(p3[:100, 0], p3[:100, 1], p3[:100, 2], c=labels[:100].astype(np.float))
+	    plt.cla()
+	    ax.scatter(p3[:1000, 0], p3[:1000, 1], p3[:1000, 2], c=labels[:1000].astype(np.float))
 
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-    fignum = fignum + 1
+	    # ax.w_xaxis.set_ticklabels([])
+	    # ax.w_yaxis.set_ticklabels([])
+	    # ax.w_zaxis.set_ticklabels([])
+	    fignum = fignum + 1
+
+def readData(matidx = 0):
+	matfilepath    = '/Users/Chenge/Desktop/testklt/'
+	matfiles       = sorted(glob.glob(matfilepath + 'klt_*.mat'))
+	start_position = 0 
+	matfiles       = matfiles[start_position:]
+	matfile = matfiles[matidx]
+	# print "Processing truncation...", str(matidx+1)
+	ptstrj = loadmat(matfile)
+	x    = csr_matrix(ptstrj['xtracks'], shape=ptstrj['xtracks'].shape).toarray()
+	y    = csr_matrix(ptstrj['ytracks'], shape=ptstrj['ytracks'].shape).toarray()
+	return x,y,ptstrj,matfile
+
+
+def getSmoothMtx(x,y):
+	x_smooth_mtx = np.zeros(x.shape)
+	y_smooth_mtx = np.zeros(y.shape)
+	for kk in range(0,x.shape[0],1):
+		xk = x[kk,:][x[kk,:]!=0]
+		yk = y[kk,:][y[kk,:]!=0]
+		if len(xk)>5 and (min(xk.max()-xk.min(), yk.max()-yk.min())>2): # range span >=2 pixels  # loger than 5, otherwise all zero out
+			x_smooth, y_smooth = smooth(xk, yk)
+			x_smooth_mtx[kk,:][x[kk,:]!=0]=x_smooth
+			y_smooth_mtx[kk,:][y[kk,:]!=0]=y_smooth
+	return x_smooth_mtx,y_smooth_mtx
+
+
+def plotTrj(x,y,Trjchoice=[]):
+	if Trjchoice==[]:
+		Trjchoice=range(x.shape[0])
+
+	plt.ion()
+	plt.figure()
+	for ii in range(0,len(Trjchoice),1):
+		kk = Trjchoice[ii]
+		xk = x[kk,:][x[kk,:]!=0]
+		yk = y[kk,:][y[kk,:]!=0]
+		if len(xk)>=5 and (min(xk.max()-xk.min(), yk.max()-yk.min())>2): # range span >=2 pixels
+			# plt.plot(xk)
+			# plt.plot(yk)
+			plt.plot(xk, yk)
+			# extraPolate(xk, yk)
+			# x_fit = np.linspace(xk.min(), xk.max(), 200)
+			# y_fit = pow(x_fit,3)*p3[ii,0] + pow(x_fit,2)*p3[ii,1] + pow(x_fit,1)*p3[ii,2]+ p3[ii,3]
+			# plt.plot(x_fit, y_fit)
+			plt.draw()
+	plt.show()
+
+
+def saveSmoothMat(x_smooth_mtx,y_smooth_mtx,p3,goodTrj,  ptstrj,matfile):
+	ptstrj['xtracks'] = csr_matrix(x_smooth_mtx)
+	ptstrj['ytracks'] = csr_matrix(y_smooth_mtx)
+	ptstrj['polyfitCoef'] = p3
+	ptstrj['goodTrjInd'] = goodTrj
+	# savename = matfile[:-4]+'_smooth'+'.mat'
+	parentPath = os.path.dirname(matfile)
+	smoothPath = os.path.join(parentPath,'smooth/')
+	if not os.path.exists(smoothPath):
+		os.mkdir(smoothPath)
+	onlyFileName = matfile[len(parentPath)+1:]
+	savename = os.path.join(smoothPath,onlyFileName[:-4]+'_smooth'+'.mat')
+	savemat(savename,ptstrj)
+
+
+
+
+def main(matidx):
+	x,y,ptstrj,matfile = readData(matidx)
+	# "if consecutive points are similar to each other, merge them, using one to represent"
+	# didn't do this, smooth and resample instead
+	x_smooth_mtx,y_smooth_mtx = getSmoothMtx(x,y)
+	# plotTrj(x_smooth_mtx,y_smooth_mtx)
+	p3,goodTrj = polyFitTrj(x_smooth_mtx,y_smooth_mtx)
+	# kmeansPolyCoeff(p3)
+
+	# plotTrj(x_smooth_mtx,y_smooth_mtx,goodTrj)
+	saveSmoothMat(x_smooth_mtx,y_smooth_mtx,p3,goodTrj,ptstrj,matfile)
+
+
+if __name__ == '__main__':		
+	# define start and end regions
+	#Canal video's dimensions:
+	"""(528, 704, 3)
+	start :<=100,
+	end: >=500,"""
+
+	start_Y = 100;
+	end_Y   = 500;
+	for matidx in range(6):
+		main(matidx)
+
+
 
 
 
