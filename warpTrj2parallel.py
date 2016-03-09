c3b79e4667fb62d3ac4cbaaf8f8b63a04208039a
ChengeLi <cl2840@nyu.edu>
2016-03-09 12:27:40 -0500

    start warp trj (not images)

--- warpTrj2parallel.py
+++ warpTrj2parallel.py
@@ -7,8 +7,6 @@ import cv2
 
 def loadWarpMtx(homography_filename):
 	# warpingMtx = np.loadtxt(homography_filename)
-
-
 	pts1 = np.float32([[242, 192],[179,500],[289,190],[447,417]])  #([x,y]) canal st, right lane
 	pts2 = np.float32([[125,97],[684,621],[179,48],[719,548]]) #canal world
 	# pts2 = np.float32([[0,0],[0,81],[81,0],[81,736]]) #canal
@@ -20,15 +18,25 @@ def loadWarpMtx(homography_filename):
 
 	M = cv2.getPerspectiveTransform(pts1,pts2)
 
-	dst = cv2.warpPerspective(frame,M,(81,736))  # canal
+	dst1 = cv2.warpPerspective(frame,M,(81,736))  # for image
 
-	dst = np.zeros((81,736))
-	for src_y in range(frame.shape[0]):
-		for src_x in range(frame.shape[1]):
-			# [src_x,src_y] = frame[ii,jj,0] 
-			dst_x = np.dot([src_x,src_y,1],M[0,:])/np.dot([src_x,src_y,1],M[2,:])	
-			dst_y = np.dot([src_x,src_y,1],M[1,:])/np.dot([src_x,src_y,1],M[2,:])
-			dst[np.floor(dst_y),np.floor(dst_x)] = frame[src_y,src_x,0]
+	dst = np.zeros((736,81,3))
+ 
+	# for src_y in range(73,399,1):
+	# 	for src_x in range(147,519,1):
+	# 		dst_x = np.dot([src_x,src_y,1],M[0,:])/np.dot([src_x,src_y,1],M[2,:])	
+	# 		dst_y = np.dot([src_x,src_y,1],M[1,:])/np.dot([src_x,src_y,1],M[2,:])
+	# 		dst[np.floor(dst_y),np.floor(dst_x)] = frame[src_y,src_x,:]
+
+
+	a = np.array([[1, 2], [4, 5], [7, 8]],dtype='float32')
+
+
+	dsttest = cv2.perspectiveTransform(np.array([trj]), np.array(M,dtype='float32'))
+
+
+
+	pdb.set_trace()
 
 
 	return warpingMtx
@@ -44,10 +52,12 @@ def warpTrj2parallel(frame, warpingMtx):
 
 
 
+
 homography_filename = '/home/chengeli/CUSP/AIG/Saunier/Canal_homographyNEW.txt'
 warpingMtx = loadWarpMtx(homography_filename)
 
-frame = cv2.imread('/home/chengeli/CUSP/AIG/Saunier/00000012.jpg')
+# frame = cv2.imread('/home/chengeli/CUSP/AIG/Saunier/00000012.jpg')
+frame = cv2.imread('../DoT/CanalSt@BaxterSt-96.106/imgs/00000011.jpg')
 world_frame = cv2.imread('/home/chengeli/CUSP/AIG/Saunier/canal_baxter_world.jpg')
 
 leftLane = frame[190:500,179:447,:]
