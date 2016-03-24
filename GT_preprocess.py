fix the lowess silent nan bug

GT_preprocess.py
@@ -4,44 +4,81 @@ import pickle as pickle
 import numpy as np
 import pdb
 
+from parameterClass import *
+Parameterobj = parameter(dataSource,VideoIndex)
+
 def readGTdata():
-	f =  open('/Users/Chenge/Desktop/GroundTruth.csv', 'rb')
-	reader = csv.reader(f)
-	subsampleRate = 4
 
-	# frame_idx = 10280
-	GTupperL_list = []
-	GTLowerR_list = []
-	GTcenterXY_list = []
-	frame_idx_list = []
-	vehicleInd_list = []
+	if dataSource == 'DoT':
+		f =  open('/Users/Chenge/Desktop/GroundTruth.csv', 'rb')
+		reader = csv.reader(f)
+		# subsampleRate = 4
 
-	frame_idx=0
-	vehicleInd = 0
-	GTtrjdic = {}
-	while frame_idx<4000:
-		temp = reader.next()
-		# for temp in reader:
-		# pdb.set_trace()
-		
-		if np.double(temp[0])<frame_idx: # new car	
-			GTtrjdic[vehicleInd] = GTtrj(GTupperL_list,GTLowerR_list,GTcenterXY_list,frame_idx_list)
-			frame_idx_list  = []
-			GTupperL_list   = []
-			GTLowerR_list   = []
-			GTcenterXY_list = []
-			vehicleInd_list = []
-			vehicleInd += 1
+		# frame_idx = 10280
+		GTupperL_list = []
+		GTLowerR_list = []
+		GTcenterXY_list = []
+		frame_idx_list = []
 
-		frame_idx = np.double(temp[0])
-		GTupperL = np.double(temp[1:3])  #upper left
-		GTLowerR = np.double(temp[3:5])  #lower right
-		GTcenterXY = np.double(temp[-2:])
-		frame_idx_list.append(frame_idx)
-		GTupperL_list.append(GTupperL)
-		GTLowerR_list.append(GTLowerR)
-		GTcenterXY_list.append(GTcenterXY)
-		return GTtrjdic
+		frame_idx=0
+		vehicleInd = 0
+		GTtrjdic = {}
+		while frame_idx<4000:
+			temp = reader.next()
+			if np.double(temp[0])<frame_idx: # new car	
+				GTtrjdic[vehicleInd] = GTtrj(GTupperL_list,GTLowerR_list,GTcenterXY_list,frame_idx_list)
+				frame_idx_list  = []
+				GTupperL_list   = []
+				GTLowerR_list   = []
+				GTcenterXY_list = []
+				vehicleInd += 1
+			frame_idx = np.double(temp[0])
+			GTupperL = np.double(temp[1:3])  #upper left
+			GTLowerR = np.double(temp[3:5])  #lower right
+			GTcenterXY = np.double(temp[-2:])
+
+			frame_idx_list.append(frame_idx)
+			GTupperL_list.append(GTupperL)
+			GTLowerR_list.append(GTLowerR)
+			GTcenterXY_list.append(GTcenterXY)
+	if dataSource == 'NGSIM':
+		f =  open('/Volumes/Transcend/US-101/US-101-MainData/vehicle-trajectory-data/0750am-0805am/trajectories-0750am-0805am.txt', 'rb')
+		reader = csv.reader(f)
+		GTupperL_list = []
+		GTLowerR_list = []
+		GTcenterXY_list = []
+		frame_idx_list = []
+
+		frame_idx=0
+		GTtrjdic = {}
+		vehicleInd = np.NaN
+		while frame_idx<4000:
+			temp = reader.next()
+			temp = temp[0].split()
+			if np.double(temp[0])!=vehicleInd: # new car
+				if not np.isnan(vehicleInd): ##save the last car info
+					plt.plot(np.array(GTcenterXY_list)[:,0],np.array(GTcenterXY_list)[:,1])
+					plt.draw()
+					plt.show()
+					GTtrjdic[vehicleInd] = GTtrj(GTupperL_list,GTLowerR_list,GTcenterXY_list,frame_idx_list)
+				vehicleInd = np.double(temp[0])	
+				frame_idx_list  = []
+				GTcenterXY_list = []
+
+
+			frame_idx = np.double(temp[1])
+			GTcenterXY = np.double(temp[4:6])
+			""""in feet!"""
+			GT_vehicle_len  = np.double(temp[8])
+			GT_vehicle_wid  = np.double(temp[9])
+
+			frame_idx_list.append(frame_idx)
+			"""(X,Y)"""
+			GTupperL_list.append((np.double(temp[4])-GT_vehicle_len/2, np.double(temp[5])-GT_vehicle_wid/2))
+			GTLowerR_list.append((np.double(temp[4])+GT_vehicle_len/2, np.double(temp[5])+GT_vehicle_wid/2))
+			GTcenterXY_list.append(GTcenterXY) 
+
+	return GTtrjdic
 
 
 
@@ -51,8 +88,6 @@ class GTtrj(object):
 		self.GTLowerR_list = GTLowerR_list
 		self.GTcenterXY_list = GTcenterXY_list
 		self.time = frame_idx_list
-
-
 
 
 
@@ -77,7 +112,7 @@ if __name__ == '__main__':
 	GTtrjdic = readGTdata()
 	pdb.set_trace()
 	GTtrjobj = GTtrj(GTupperL_list,GTLowerR_list,GTcenterXY_list,frame_idx_list,vehicleInd_list)
-	pickle.dump(GTtrjobj,open('./Canal_GTtrjobj','wb'))
+	pickle.dump(GTtrjobj,open('./GTtrjobj'+	Parameterobj.dataSource,'wb'))
 
 
 

fit_extrapolate.py
@@ -78,9 +78,8 @@ def polyFitTrj_filtering(x,y,xspd_mtx,yspd_mtx):
 		# xaccelerate = np.diff(xspd)
 		# yaccelerate = np.diff(yspd)
 
-		xspd = xspd_mtx[kk,:][np.where(x[kk,:]!=0)[0][1:]]
-		yspd = yspd_mtx[kk,:][np.where(x[kk,:]!=0)[0][1:]]
-
+		xspd = xspd_mtx[kk,:][x[kk,:]!=0][1:]
+		yspd = yspd_mtx[kk,:][y[kk,:]!=0][1:]
 		# print sum(xspd)
 		# print sum(accelerate)
 		satisfyCriterion = filteringCriterion(xk,yk,xspd,yspd)
@@ -208,19 +207,25 @@ def lowessSmooth(xk,yk):
 
 
 	#fit x(t) and y(t) seperately
-	lowessX = sm.nonparametric.lowess(xk,range(len(xk)), frac=0.1)
+	lowessX = sm.nonparametric.lowess(xk,range(len(xk)), frac=max(2.0/len(xk),0.1))
 	# plt.figure('smooth X(t)')
 	# plt.plot(range(len(xk)), xk, '+')
 	# plt.plot(lowessX[:, 0], lowessX[:, 1])
 	# plt.show()
 	xk_smooth = lowessX[:, 1]
-
-	lowessY = sm.nonparametric.lowess(yk,range(len(yk)), frac=0.1)
+	lowessY = sm.nonparametric.lowess(yk,range(len(yk)), frac=max(2.0/len(xk),0.1))
 	# plt.figure('smooth Y(t)')
 	# plt.plot(range(len(yk)), yk, '+')
 	# plt.plot(lowessY[:, 0], lowessY[:, 1])
 	# plt.show()
 	yk_smooth = lowessY[:, 1]
+	# if np.sum(np.isnan(xk_smooth))>0:
+	# 	print 'X nan!!'
+	# if np.sum(np.isnan(yk_smooth))>0:
+	# 	print 'Y nan!!'
+	"""lowess returns nan and does not warn if there are too few neighbors!"""
+	xk_smooth[np.isnan(xk_smooth)] = xk[np.isnan(xk_smooth)]
+	yk_smooth[np.isnan(yk_smooth)] = yk[np.isnan(yk_smooth)]
 	return xk_smooth, yk_smooth
 
 def getSpdMtx(dataMtx_withnan):
@@ -242,14 +247,12 @@ def getSmoothMtx(x,y):
 			x_spatial_smooth, y_spatial_smooth = smooth(xk, yk)
 			x_time_smooth, y_time_smooth = lowessSmooth(xk, yk)
 
-
 			x_spatial_smooth_mtx[kk,:][x[kk,:]!=0]=x_spatial_smooth
 			y_spatial_smooth_mtx[kk,:][y[kk,:]!=0]=y_spatial_smooth
 
 			x_time_smooth_mtx[kk,:][x[kk,:]!=0]=x_time_smooth
 			y_time_smooth_mtx[kk,:][y[kk,:]!=0]=y_time_smooth
 
-	'dont use loop, use mtx diff'
 	xspd_smooth_mtx = getSpdMtx(x_time_smooth_mtx)
 	yspd_smooth_mtx = getSpdMtx(y_time_smooth_mtx)
 	x_time_smooth_mtx[np.isnan(x_time_smooth_mtx)]=0  # change nan back to zero for sparsity
@@ -292,9 +295,6 @@ def plotTrj(x,y,p3=[],Trjchoice=[]):
 			plt.draw()
 	plt.show()
 	pdb.set_trace()
-
-
-
 
 
 def saveSmoothMat(x_smooth_mtx,y_smooth_mtx,xspd_smooth_mtx,yspd_smooth_mtx,p3,goodTrj,ptstrj,matfile):

foreground_blob.py
@@ -48,7 +48,6 @@ if __name__ == '__main__':
 	matfiles,offset = readData()
 	frame_idx = 0 
 	for matidx, matfile in enumerate(matfiles):
-		pdb.set_trace()
 	# for	matidx in range(1,len(matfiles)):
 		# matfile = matfiles[matidx]
 		try:  #for matfile <-v7.3
@@ -74,15 +73,15 @@ if __name__ == '__main__':
 			# mask    = cv2.imread(mask_list[(frame_idx*subSampRate)/choice_Interval])
 			ImgSlice = (mask_tensor[frame_idx*subSampRate,:].reshape((Ncols,Nrows))).transpose() #Careful!! Warning! It's transposed!
 			maskgray = ImgSlice
-			plt.imshow(np.uint8(ImgSlice))
+			plt.imshow(np.uint8(ImgSlice),cmap = 'gray')
 			plt.draw()
-			
+			plt.pause(0.0001)
+			pdb.set_trace()
 			"""use ndimage.measurements"""
 			blobLabelMatrix, BlobCenters = blobImg2blobmatrix(maskgray)
 			sparse_slice = csr_matrix(blobLabelMatrix)
 			blobLabelMtxList.append(sparse_slice)
 			blobCenterList.append(BlobCenters)
-			pdb.set_trace()
 			frame_idx = frame_idx+1
 			#end of while loop
 			if ((frame_idx>0) and (np.mod(frame_idx,trunclen)==0)) or (frame_idx*subSampRate==mask_tensor.shape[0]):

subspace_clustering_merge.py
@@ -165,8 +165,7 @@ def uniqulizeLabel(labels):
 
 def ssc_with_Adj_CC(trjAdj,CClabel,trjID):
     small_connected_comp = []
-    one_car_trjID = pickle.load(open('./johnson_one_car_trjID','rb'))
-
+    # one_car_trjID = pickle.load(open('./johnson_one_car_trjID','rb'))
 
     labels_DPGMM = np.ones(CClabel.size)*(-222)
     labels_spectral = np.ones(CClabel.size)*(-222)
@@ -178,9 +177,9 @@ def ssc_with_Adj_CC(trjAdj,CClabel,trjID):
     FeatureMtx = pickle.load(open('./Johnson00115_FeatureMtx', 'rb'))
     FeatureMtx[np.isnan(FeatureMtx)] = 0
     
-    FeatureMtxLoc = []
-    for aa in one_car_trjID:
-        FeatureMtxLoc+=list(np.where(trjID[0]==aa)[0])
+    # FeatureMtxLoc = []
+    # for aa in one_car_trjID:
+    #     FeatureMtxLoc+=list(np.where(trjID[0]==aa)[0])
 
     for i in np.unique(CClabel):
         color = ((color_choice[i].T) / 255.)

trjcluster_func_SBS.py
@@ -337,12 +337,20 @@ def get_hue_diff(hue_i,hue_j):
     huedis = np.abs(np.mean(hue_i)-np.mean(hue_j))
     return huedis
 
+def diff_feature_on_one_car(dataForKernel,trjID):
+    one_car_trjID = pickle.load(open('./johnson_one_car_trjID','rb'))
+    FeatureMtx = pickle.load(open('./Johnson00115_FeatureMtx', 'rb'))
+    FeatureMtx[np.isnan(FeatureMtx)] = 0
+
+    FeatureMtxLoc = []
+    for aa in one_car_trjID:
+        FeatureMtxLoc+=list(np.where(trjID[0]==aa)[0])
+    pdb.set_trace()
 
 
 
 if __name__ == '__main__':
     isVisualize = False
-
     matfiles,savePath,start_position_offset = prepare_input_data()
     # adj_methods = np.nan
     # adj_methods = "Thresholding"
@@ -436,7 +444,6 @@ if __name__ == '__main__':
                 continue
 
             print'building adj mtx ....', NumGoodsampleSameDir,'*',NumGoodsampleSameDir
-            # pdb.set_trace()
             dataForKernel = np.array([x[sameDirInd,:],y[sameDirInd,:],xspd[sameDirInd,:],yspd[sameDirInd,:],hue[sameDirInd,:],fg_blob_center_X[sameDirInd,:],fg_blob_center_Y[sameDirInd,:]])
             FeatureMtx,FeatureMtx_norm = getRawDataFeatureMtx(dataForKernel)
             # TwoD_Emedding(FeatureMtx_norm)
@@ -447,6 +454,13 @@ if __name__ == '__main__':
 
             if adj_methods =="Gaussian":
                 mean_std_ForKernel,extremeValue = getMuSigma(dataForKernel)
+
+            """test only for one car, see different features' role in the adj"""
+            # diff_feature_on_one_car(dataForKernel,trjID)
+
+
+
+
             # build adjacent mtx
             for i in range(NumGoodsampleSameDir):
                 print "i", i