908d45d727270565a665cfbf2cb5f58b631260af
ChengeLi <lichenge0223@gmail.com>
2016-03-24 22:46:53 -0400

    gif, but not working yet

--- plot2gif.py
+++ plot2gif.py
@@ -1,12 +1,11 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from moviepy.video.io.bindings import mplfig_to_npimage
-# import moviepy.editor as mpy
+import matplotlib.pyplot as plt
+import numpy as np
+from moviepy.video.io.bindings import mplfig_to_npimage
+import moviepy.editor as mpy
+import pdb
+import cv2
 
-# matplotlib.use('TkAgg')
-
-
-# # DRAW A FIGURE WITH MATPLOTLIB
+# DRAW A FIGURE WITH MATPLOTLIB
 
 # duration = 2
 
@@ -17,50 +16,70 @@
 # ax.set_ylim(-1.5,2.5)
 # line, = ax.plot(xx, zz(0), lw=3)
 
-# # ANIMATE WITH MOVIEPY (UPDATE THE CURVE FOR EACH t). MAKE A GIF.
+filenameList = sorted(glob.glob('/home/chengeli/Downloads/vis/vis/*.jpg'))
+duration = len(filenameList)
+fig_mpl,ax = plt.subplots(1)#,figsize=(im.height, im.width))
 
-# def make_frame_mpl(t):
-#     line.set_ydata( zz(2*np.pi*t/duration))  # <= Update the curve
-#     return mplfig_to_npimage(fig_mpl) # RGB image of the figure
+def make_frame_mpl(t):
+    # line.set_ydata( zz(2*np.pi*t/duration))  # <= Update the curve
+	# im = Image.open(filenameList[int(t)])
+	im = cv2.imread(filenameList[int(t)])
+	print int(t)
+	# original_duration = im.info['duration']
+	# frames = [frame.copy() for frame in ImageSequence.Iterator(im)]    
+	# plt.imshow(im)
+	plt.imshow(im)
+	return mplfig_to_npimage(fig_mpl) # RGB image of the figure
 
-# animation =mpy.VideoClip(make_frame_mpl, duration=duration)
-# animation.write_gif("sinc_mpl.gif", fps=20)
 
 
-import numpy as np
-import matplotlib.pyplot as plt
-from moviepy.video.io.bindings import mplfig_to_npimage
-import moviepy.editor as mpy
+# ANIMATE WITH MOVIEPY (UPDATE THE CURVE FOR EACH t). MAKE A GIF.
+# import glob as glob
+# from PIL import Image, ImageSequence
 
-fig = plt.figure(facecolor="white") # <- ADDED FACECOLOR FOR WHITE BACKGROUND
-ax = plt.axes()
-x = np.random.randn(10, 1)
-y = np.random.randn(10, 1)
-p = plt.plot(x, y, 'ko')
-time = np.arange(2341973, 2342373)
+# filename = sys.argv[1]
 
-last_i = None
-last_frame = None
+fps = 50
+animation =mpy.VideoClip(make_frame_mpl, duration=100)
+pdb.set_trace()
+# animation =mpy.VideoClip(mplfig_to_npimage(fig),duration=duration)
+animation.write_gif("~/Desktop/NGSIMvis.gif", fps=fps)
 
-def animate(t):
-    global last_i, last_frame
 
-    i = int(t)
-    if i == last_i:
-        return last_frame
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from moviepy.video.io.bindings import mplfig_to_npimage
+# import moviepy.editor as mpy
 
-    xn = x + np.sin(2 * np.pi * time[i] / 10.0)
-    yn = y + np.cos(2 * np.pi * time[i] / 8.0)
-    p[0].set_data(xn, yn)
+# fig = plt.figure(facecolor="white") # <- ADDED FACECOLOR FOR WHITE BACKGROUND
+# ax = plt.axes()
+# x = np.random.randn(10, 1)
+# y = np.random.randn(10, 1)
+# p = plt.plot(x, y, 'ko')
+# time = np.arange(2341973, 2342373)
 
-    last_i = i
-    last_frame = mplfig_to_npimage(fig)
-    return last_frame
+# last_i = None
+# last_frame = None
 
-duration = len(time)
-fps = 15
-animation = mpy.VideoClip(animate, duration=duration)
-animation.write_videofile("test.mp4", fps=fps)
+# def animate(t):
+#     global last_i, last_frame
+
+#     i = int(t)
+#     if i == last_i:
+#         return last_frame
+
+#     xn = x + np.sin(2 * np.pi * time[i] / 10.0)
+#     yn = y + np.cos(2 * np.pi * time[i] / 8.0)
+#     p[0].set_data(xn, yn)
+
+#     last_i = i
+#     last_frame = mplfig_to_npimage(fig)
+#     return last_frame
+
+# duration = len(time)
+# fps = 15
+# animation = mpy.VideoClip(animate, duration=duration)
+# animation.write_videofile("test.mp4", fps=fps)
 
 
 
